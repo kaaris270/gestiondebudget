d5df3510ad5c2699a0990ea4ea27b81da0fe38a3
kaaris270 <85549480+kaaris270@users.noreply.github.com>
2022-05-12 16:55:27 +0200

    Add files via upload

---
+++ components/Operationpo.js
@@ -0,0 +1,28 @@
+
+function Operationpo() {
+    return (
+        <div className="Operationpo">
+            <div className="container">
+                <div className="row my-2">
+                    <div className="col-4 d-flex justify-content-center">
+                        <p>
+                        LUN 2 MAI 2022
+                        </p>
+                    </div>
+                    <div className="col-4 opacity-50 d-flex justify-content-center">
+                        <p className="text-center">
+                        LW SNCF CONNECT ACHAT BILLET TRAJET 03/05/2022
+                        </p>
+                    </div>
+                    <div className="col-4 rouge d-flex justify-content-center">
+                        <p className="rouge">
+                        -199,99 €
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Operationpo;
